3e6421e5e48529016ba2c8bf1d293e698514d4af
Ivan <ivanvlora@gmail.com>
2019-03-10 00:11:04 +0900

    modified line due to old syntax

--- db/migrate/20190124071625_add_status_to_posts.rb
+++ db/migrate/20190124071625_add_status_to_posts.rb
@@ -1,5 +1,5 @@
 class AddStatusToPosts < ActiveRecord::Migration[5.2]
   def change
-    add_column :posts, :status, :integer, :default=> 0
+    add_column :posts, :status, :integer, default: 0
   end
 end
