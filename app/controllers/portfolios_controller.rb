28ff19d8311093c641b67d3958b7da943e39c7ef
Ivan <ivanvlora@gmail.com>
2019-03-21 18:15:11 +0900

    Implemented configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,55 +1,58 @@
 class PortfoliosController < ApplicationController
-    def index
-      @portfolio_items = Portfolio.all
-    end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-    def react
-      @react_portfolio_items = Portfolio.react
-    end
+  def react
+    @react_portfolio_items = Portfolio.react
+  end
 
-    def new
-      @portfolio_item = Portfolio.new
-    end
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
 
-    def create
-      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(
+      :title, :subtitle, :body, technologies_attributes: [:name]
+      ))
 
-      respond_to do |format|
-        if @portfolio_item.save
-          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.'}
-        else
-          format.html { render :new}
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.'}
+      else
+        format.html { render :new}
       end
     end
+  end
 
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-    def update
-      @portfolio_item = Portfolio.find(params[:id])
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
 
-      respond_to do |format|
-        if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-          format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
-        else
-          format.html { render :edit}
-        end
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit}
       end
     end
-    
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully deleted.' }
     end
-
-    def destroy
-      @portfolio_item = Portfolio.find(params[:id])
-
-      @portfolio_item.destroy
-
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully deleted.' }
-      end
-    end
+  end
 end
