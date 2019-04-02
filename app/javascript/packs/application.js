79f1ed4387007c76676000282abf9766e9f0b45a
Ivan <ivanvlora@gmail.com>
2019-04-02 14:01:09 +0900

    fixed a bug on the blog page due to title being nil on show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,15 @@
 document.addEventListener('turbolinks:load', function() {
-  console.log('ok');
   var elems = document.querySelectorAll('.dropdown-trigger');
   var options = {
     hover: true
   };
   var instances = M.Dropdown.init(elems, options);
 
-  particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+  particlesJS.load(
+    'particles-js',
+    'assets/javascript/particles.json',
+    function() {
+      console.log('callback - particles.js config loaded');
+    }
+  );
 });
